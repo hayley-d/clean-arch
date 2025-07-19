5caffe51450bb482ef69baa880fa7c8fdde93942
hayley-d <hayley@codehesion.com>
2025-07-19 22:59:57 +0200

    add validator and handler

--- src/GradTest.Application/BoundedContexts/Orders/Queries/GetOrderByIdQuery.cs
+++ src/GradTest.Application/BoundedContexts/Orders/Queries/GetOrderByIdQuery.cs
@@ -1,6 +1,19 @@
+using GradTest.Contracts.Orders.Responses;
+using MediatR;
+
 namespace GradTest.Application.BoundedContexts.Orders.Queries;
 
 public class GetOrderByIdQuery
+{
+    
+}
+
+public class GetOrderByIdQueryValidator : IQuery<Result<OrderResponse>>
+{
+    
+}
+
+public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, Result<OrderResponse>>
 {
     
 }
